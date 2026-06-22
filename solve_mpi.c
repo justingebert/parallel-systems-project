b5a4e1fbebeabc96aa5f5dc4059f69b4b2a4aee4
Pascal Seifert <seifert.pascal@web.de>
2026-06-22 17:50:49 +0000

    feat: generate and build jobs pool for MPI processes

--- solve_mpi.c
+++ solve_mpi.c
@@ -1,35 +1,61 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "solve_mpi.h"
 #include "cube.h"
 
-bool initMpi(CubeState cube, int length) {
-  int rank, size;
-  const int maxnamelen = MPI_MAX_PROCESSOR_NAME;
-  int resultlen = maxnamelen;
+#define JOB_DEPTH 2
 
-  char name[maxnamelen];
+static int generateMPIJobs(CubeState cube, int currentDepth, int remainingDepth, MPISearchJob *jobs) {
+    if (currentDepth == 0) {
+        jobs[0].cube = cube;
+        jobs[0].remainingDepth = remainingDepth;
 
-  MPI_Init(NULL, NULL);
+        return 1;
+    }
 
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Get_processor_name(name, &resultlen);
-
-
-  if (rank == 0) {
     CubeExpansion expansion = expand(cube);
-    int chunk = CUBE_MOVE_COUNT + size / size;
-    int total = chunk * size;
+    int count = 0;
 
-    
-  } else {
-    
-  }
+    for (int i = 0; i < CUBE_MOVE_COUNT; i++) {
+        count += generateMPIJobs(expansion.states[i], currentDepth - 1, remainingDepth - 1, jobs + count);
+    }
 
-  printf("Hello World from process %d of %d, and my name is %s\n", rank, size, name);
-  
-  MPI_Finalize();
+    return count;
+}
+
+
+bool initMpi(CubeState cube, int length) {
+    MPI_Init(NULL, NULL);
+
+    int rank, size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    MPISearchJob *allJobs = NULL;
+    int totalJobs = 0;
+
+    if (rank == 0) {
+        int maxJobs = 1;
+        for (int i = 0; i < JOB_DEPTH; i++) {
+            maxJobs *= CUBE_MOVE_COUNT;
+        }
+
+        allJobs = malloc(sizeof(*allJobs) * (size_t)maxJobs);
+
+        totalJobs = generateMPIJobs(cube, JOB_DEPTH, length, allJobs);
+
+        printf("Generated %d jobs across %d MPI ranks\n", totalJobs, size);
+
+        free(allJobs);
+    } else {
+        printf("Hello World from process %d of %d\n", rank, size);
+    }
+
+
+    MPI_Finalize();
+    return true;
 }
 
--- solve_mpi.h
+++ solve_mpi.h
@@ -2,6 +2,12 @@
 #define SOLVE_MPI_H
 
 #include "cube.h"
+#include <stdbool.h>
+
+typedef struct {
+    CubeState cube;
+    int remainingDepth;
+} MPISearchJob;
 
 bool initMpi(CubeState cube, int length);
 
